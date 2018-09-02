f975a616c4bc60b50f9864492b76ca985fdc0b24
Chris Sprague <chris@chaoscontrol.org>
2018-09-01 20:51:08 -0400

    Move initialization to Board class

--- run.php
+++ run.php
@@ -4,10 +4,6 @@ spl_autoload_register(function ($class) {
     include $class . '.php';
 });
 
-$board = new Board(25, 25);
-
-while (true) {
-    $board->render();
-    $board->update();
-
-}
+// Board() takes 2 optional paramaters to set the height (int) and width (int).
+$board = new Board();
+$board->run();
